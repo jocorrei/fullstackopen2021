832edbad173e65c78ad639fdb1b8cfccd48c1c43
root <root@DESKTOP-JOHV3EV.localdomain>
2022-01-15 16:54:26 +0000

    ex4.2

--- part04/controllers/blogs.js
+++ part04/controllers/blogs.js
@@ -7,11 +7,50 @@ blogRouter.get('/', (request, response) => {
 	})
 })
 
+blogRouter.get('/:id', (request, response) => {
+//	console.log(request.params);
+	Blog
+	.findById(request.params.id)
+	.then(resp => {
+		response.json(resp)
+	})
+})
+
+blogRouter.delete('/:id', (request, response) => {
+	Blog
+	.findByIdAndDelete(request.params.id)
+	.then(resp => {
+		if (resp) {
+			response.json(resp)
+		}
+		else {
+			response.status(404).send({error: 'inexisting ID'})
+		}
+	})
+})
+
 blogRouter.post('/', (request, response) => {
 	const blog = new Blog(request.body)
 
 	blog.save().then(result => {
 		response.status(201).json(result)
+	})
+})
+
+blogRouter.put('/:id', (request, response, next) => {
+	
+	console.log(request.body);
+
+	Blog.findByIdAndUpdate(request.params.id, request.body, {
+		new:true, runValidators:true
+	})
+	.then(resp => {
+		if (resp) {
+			response.json(resp)
+		}
+		else {
+			response.status(404).send({error: 'Couldnt find entry'})
+		}
 	})
 })
 
